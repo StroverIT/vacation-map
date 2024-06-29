5596ad4d7a9a55a78a2745905fa8347f0904a572
StroverIT <xcreaw1@abv.bg>
2024-06-29 17:56:30 +0300

    Adding google maps; Data is showing one by one

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,28 @@
 import ListLabels from "@/components/ListLabels";
+import GoogleMap from "@/components/Map";
 import { promises as fs } from "fs";
 // import { formatTrelloData } from "@/utils/formatDocument";
 
 export default async function Home() {
+  // formatTrelloData({ lists, cards });
+
   const file = await fs.readFile(process.cwd() + "/data/places.json", "utf8");
   const { lists, cards } = JSON.parse(file);
 
-  // formatTrelloData({ lists, cards });
-
-  const findListCardsById = (id: string) => {
-    return cards.filter((card) => card.idList === id);
-  };
   return (
     <main className="container  text-center">
       <h1 className="text-4xl text-center my-4">Места за посещаване</h1>
-      <div className="grid grid-cols-2 gap-4">
-        {lists.map((list) => (
-          <ListLabels
-            key={list.id}
-            data={list}
-            findListCardsById={findListCardsById}
-          />
-        ))}
+      <div className="grid grid-cols-2 self-start">
+        <div className="grid gap-4">
+          {lists.map((list) => (
+            <ListLabels
+              key={list.id}
+              data={list}
+              cards={cards}
+            />
+          ))}
+        </div>
+        <GoogleMap />
       </div>
     </main>
   );
