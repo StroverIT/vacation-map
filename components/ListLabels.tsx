51bd334332df976f49121dcaa6dac434c07d775e
StroverIT <xcreaw1@abv.bg>
2024-06-29 17:55:37 +0300

    Showing and hiding the current cards

--- components/ListLabels.tsx
+++ components/ListLabels.tsx
@@ -1,22 +1,37 @@
+"use client"
+
+import { useMemo, useState } from "react";
+
 export default function ListLabels({
   data,
   findListCardsById,
+  cards
 }: {
   data: any;
   findListCardsById: any;
 }) {
-  const cards = findListCardsById(data.id);
+  const [isHidden, setIsHidden] = useState(true);
+
+  const foundCards = useMemo(()=>{
+      return cards.filter((card) => card.idList === data.id);
+  },[cards])
+  
+  const openCloseMenuHandler = () =>{
+    setIsHidden(!isHidden)
+  }
   return (
     <div key={data.id} className="border p-4">
-      <h2 className="text-2xl font-semibold">{data.name}</h2>
-      {cards.map((card) => {
-        return (
-          <div key={card.id} className="border p-4 my-4">
-            <h3 className="">{card.name}</h3>
-            <p>{card.desc}</p>
-          </div>
-        );
-      })}
+      <h2 className="text-2xl font-semibold" onClick={openCloseMenuHandler}>{data.name}</h2>
+      <div className={`${isHidden ? "hidden" : ""}`}>
+        {foundCards.map((card) => {
+          return (
+            <div key={card.id} className="border p-4 my-4">
+              <h3 className="">{card.name}</h3>
+              <p>{card.desc}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
